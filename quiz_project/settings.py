f51a34945108df65c16f5a63f723a1b2fbab9742
Justintochukwu <justintochukwu00@gmail.com>
2025-09-11 21:09:54 +0100

    Commit to render

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -87,7 +87,9 @@ WSGI_APPLICATION = 'quiz_project.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}", conn_max_age=600
+        default=config("DATABASE_URL", default="sqlite:///db.sqlite3"), 
+        conn_max_age=600,
+        ssl_require=False
     )
 }
 
